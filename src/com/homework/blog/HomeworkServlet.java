3a707b4c2347fb2f4674610f763807c7a6bba7ed
Quinn Z <Quinn Z>
2017-02-15 15:10:21 -0600

    renamed servlet class

--- src/com/homework/blog/HomeworkServlet.java
+++ src/com/homework/blog/HomeworkServlet.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class _61L_hw1Servlet extends HttpServlet {
+public class HomeworkServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
