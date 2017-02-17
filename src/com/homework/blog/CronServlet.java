7c82677c665ac0b3cb0ec0dc62e32aff5a879b11
Brent Atchison <atchionbrent@gmail.com>
2017-02-16 22:48:07 -0600

    SendGrid account information and skeleton code

--- src/com/homework/blog/CronServlet.java
+++ src/com/homework/blog/CronServlet.java
@@ -7,23 +7,21 @@ import javax.servlet.http.*;
 import com.sendgrid.*;
 
 @SuppressWarnings("serial")
-public class GAEJCronServlet extends HttpServlet {
-	private static final Logger _logger = Logger.getLogger(GAEJCronServlet.class.getName());
+public class CronServlet extends HttpServlet {
+	private static final Logger _logger = Logger.getLogger(CronServlet.class.getName());
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 	throws IOException {
 		try { _logger.info("Cron Job has been executed");
 
-		//Put your logic here
-		//BEGIN
-		// set credentials
-		Sendgrid mail = new Sendgrid("<sendgrid_username>","<sendgrid_password>");
-
-		// set email data
-		mail.setTo("foo@bar.com").setFrom("me@bar.com").setSubject("Subject goes here").setText("Hello World!").setHtml("<strong>Hello World!</strong>");
-
-		// send your message
-		mail.send();
-		//END
+		/* set credentials */
+		Sendgrid mail = new Sendgrid("atchisonbrent","m%JQWXl2");
+		
+		/* Loop through subscriber list, sending emails */
+		for (int i = 0; i < numSubscribers; i++) {
+			/* Set email data */
+			mail.setTo(subscriber.getEmail()).setFrom("homeworkBlog@gmail.com").setSubject("Recent Posts on Blog").setText(blog.getRecentPosts()).setHtml("<strong>Hello World!</strong>");
+			mail.send();
+		}
 		
 		} catch (Exception ex) {
 		//Log any exceptions in your Cron Job
